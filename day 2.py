195c5949b1e6317d6dd028ee5647562c26d95605
Kailai-13 <kailainathan2006@gmail.com>
2026-07-14 12:46:50 +0530

    more polished

--- day 2.py
+++ day 2.py
@@ -1,7 +1,7 @@
 #messy code
 def process_students(students):
     result = []
-
+    summ =0
     for student in students:
 
         if student["marks"] >= 50:
@@ -12,14 +12,17 @@ def process_students(students):
 
             grade = "Fail"
 
+
         print(student["name"], grade)
+
+        summ+=student["marks"]
 
         result.append({
             "name": student["name"],
             "grade": grade
         })
 
-    return result
+    return result,summ/len(students)
 
 #documented code
 type studentData = dict[str,int]
@@ -34,22 +37,26 @@ def is_pass(marks :int)-> bool:
 
 def process_current_student(student : studentData)-> studentResult:
 
-    """takes one student object and assigns grade to the corresponding name ,prints it and returns it"""
-
+    """takes one student object, adds the mark to summation and assigns grade to the corresponding name ,prints it and returns it"""
+    summation+=student["marks"]
     grade = "Pass" if is_pass(student["marks"]) else "Fail"
     print(student["name"],grade)
     return {"name": student["name"],"grade":grade}
 
+def calculate_average(summation):
+    """takes summation as input but returns average based on function variables"""
+    return summation/len(students)
 
 def process_all_students(students :list[studentData])-> list[studentResult]:
 
-    """goes through students one by one and assigns grades using above function, adds it and returns it"""
-
+    """goes through students one by one and assigns grades using above function, adds it and returns it with average"""
+    summation=0
     result=[]
     for student in students:
         student_result=process_current_student(student)
         result.append(student_result)
-    return result
+    average=calculate_average(summation)
+    return result,average
 
 
 #data we need
